6a57ac6bd5e8cbcdfe935db1cc2b0036f6d3d32d
unknown <serg.lutokhin@gmail.com>
2023-08-10 14:13:16 +0300

    12 increment #2

--- internal/agent/post/post_test.go
+++ internal/agent/post/post_test.go
@@ -20,6 +20,7 @@ func TestPostCounter(t *testing.T) {
 	ListGauge[2] = GaugeMetric{"BuckHashSys", Gauge(6)}
 
 	ListCounter[1] = CounterMetric{"PollCount", Counter(100)}
+	updateTyp := "update"
 
 	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		//		fmt.Fprintf("NewServer w=%v expected=%v\n", w, expected)
@@ -28,7 +29,7 @@ func TestPostCounter(t *testing.T) {
 	defer svr.Close()
 	fmt.Printf("svr.URL=%s\n", svr.URL)
 	c := NewClient(svr.URL)
-	err := c.PostCounter(ListGauge, ListCounter)
+	err := c.PostCounter(ListGauge, ListCounter, updateTyp)
 	if err != nil {
 		t.Errorf("expected err to be nil got %v", err)
 	}
